4334e3d67a9c5a4e7004f4bac23b77051250ebaa
xtal0 <barnescrys@gmail.com>
2014-05-23 11:16:37 -0700

    Updates from exercise 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,34 @@ module.exports = function(grunt) {
 	        release: {
 	            files: [ { src: './**', dest: './dist/' } ]
 	        }
-	    }
+	    },
+		version: {
+			release: {
+			    options: {
+			        buildNumber: "1.0." + (+new Date())
+			    }				
+			},
+			dev: {
+		        options: {
+		            buildNumber: function() {
+		                var package = JSON.parse(grunt.file.read('./package.json'));
+		                var chunks = package.version.split('.');
+		                chunks[1] = parseInt(chunks[1], 10) + 1;
+		                return chunks.join('.') + '-pre';
+		            }()
+		        }
+		    }
+		}
 	});
 
 	require('load-grunt-tasks')(grunt);
 
-	grunt.registerTask('default', ['clean', 'copy']);
+	/*grunt.registerMultiTask('version', 'Add build number to the package.json', function() {
+		var package = JSON.parse(grunt.file.read('./package.json'));
+		package.version = this.options().buildNumber;
+
+		grunt.file.write('./dist/package.json', JSON.stringify(package, null, 2));
+	});*/
+
+	grunt.registerTask('default', ['clean', 'copy', 'version:release']);
 };
