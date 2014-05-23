7935c5760f6685bd8aa0b2ff01bb42500d2a620d
xtal0 <barnescrys@gmail.com>
2014-05-23 10:09:20 -0700

    commit from exercise 1

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		clean: ['./dist'],
+		copy: {
+	        release: {
+	            files: [ { src: './**', dest: './dist/' } ]
+	        }
+	    }
+	});
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.registerTask('default', ['clean', 'copy']);
+};
